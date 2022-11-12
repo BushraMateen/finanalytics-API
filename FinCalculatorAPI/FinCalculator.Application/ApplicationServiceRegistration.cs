280a098a43edfc6d02b9f956f2cda6976603451a
Mastan Patel <m.mastanpatel@gmail.com>
2022-11-12 16:30:31 +0530

    added configuration and dependency inversion

---
+++ FinCalculatorAPI/FinCalculator.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using MediatR;
+
+namespace FinCalculator.Application
+{
+    public static class ApplicationServiceRegistration 
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
---
+++ FinCalculatorAPI/FinCalculator.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using FinCalculator.Application.Contracts.Persistance;
+using FinCalculator.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinCalculator.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<FinCalculatorDbContext>(options =>
+                    options.UseSqlServer(configuration.GetConnectionString("FinCoonectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+
+
+            services.AddScoped<IPortfolioRepository, PortfolioRepository>();
+
+
+            return services;
+
+        }
+
+    }
+}
